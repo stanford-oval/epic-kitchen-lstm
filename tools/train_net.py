855f580c2933c27e7baa754a2e93a6f11f66506d
Jackie Yang <jackie@jackieyang.me>
2020-12-09 15:33:03 -0800

    re-enable train

--- tools/train_net.py
+++ tools/train_net.py
@@ -460,13 +460,13 @@ def train(cfg):
         # Shuffle the dataset.
         loader.shuffle_dataset(train_loader, cur_epoch)
         # Train for one epoch.
-        # train_epoch(train_loader, model, optimizer, train_meter, cur_epoch, cfg)
+        train_epoch(train_loader, model, optimizer, train_meter, cur_epoch, cfg)
 
         # Compute precise BN stats.
-        # if cfg.BN.USE_PRECISE_STATS and len(get_bn_modules(model)) > 0:
-        #     calculate_and_update_precise_bn(
-        #         train_loader, model, cfg.BN.NUM_BATCHES_PRECISE
-        #     )
+        if cfg.BN.USE_PRECISE_STATS and len(get_bn_modules(model)) > 0:
+            calculate_and_update_precise_bn(
+                train_loader, model, cfg.BN.NUM_BATCHES_PRECISE
+            )
 
         # Save a checkpoint.
         if cu.is_checkpoint_epoch(cur_epoch, cfg.TRAIN.CHECKPOINT_PERIOD):
